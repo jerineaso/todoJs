9dba18d089ebd818d898b61c5bcbc47c91c0828f
Jerin Easo Regi <jerin.easo.20@gmail.com>
2020-11-23 22:06:20 +0530

    Updated

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const inputBtn = document.querySelector('#inputBtn')  
-let todoList = document.querySelector('.todoList')
+const inputBtn = document.querySelector("#inputBtn");
+let todoList = document.querySelector(".todoList");
+let todoItems = [];
+let item = document.querySelector('#inputText');
 
-
-// Without using array
-// inputBtn.addEventListener('click',() =>{
-//     let item = document.querySelector('#inputText').value
-//     console.log(item)   
-//     todoList.innerHTML += "<li class = 'todoLinks'>"+item+"</li>"
-//     document.querySelector('#inputText').value = ''
-// })
-
-// Using Array
-let todoItems = ["hello","hai"];
-todoItems.forEach(displayArray)
-
-inputBtn.addEventListener('click',() =>{
-    let item = document.querySelector('#inputText').value
-    todoItems.push(item)
-    displayArray(item)
-    document.querySelector('#inputText').value = ''
+inputBtn.addEventListener('click',e=>{
+    e.preventDefault();
+    console.log('clicked');
+    console.log(item.value)
+    displayArray(item.value);
+    item.value = ''
 })
 
-function displayArray(item){
-    todoList.innerHTML += "<li class = 'todoLinks'>"+item+"</li>"
+function displayArray(items){
+    todoItems.push(items);
+    console.log(todoItems)
+    let links = document.createElement('li'); 
+    links.classList.add('todoLinks');
+    
+    let value = document.createTextNode(items);
+    links.appendChild(value)
+    todoList.appendChild(links) 
+    var myNodelist = document.getElementsByTagName("li");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+    }
 }
-
